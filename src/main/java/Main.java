506c58ab1ae41ae26e52b146f0fb86223fb0fa7f
KosKazzz <xuzzzz@gmail.com>
2022-11-17 22:31:54 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bin = new BinOps();
+        System.out.println(bin.sum("1001", "1011")); // 9 + 11 = 20
+        System.out.println(bin.mult("1001", "1011")); // 9 + 11 = 99
+    }
+}
